d33023e6df0bf8e0857fecd28845890d4eafdc36
whdal <dbswhdals2002z@gmail.com>
2025-12-31 14:43:42 +0900

    refactor

--- src/main/java/com/example/repetitivecrud/dto/ApiResponse.java
+++ src/main/java/com/example/repetitivecrud/dto/ApiResponse.java
@@ -26,7 +26,7 @@ public class ApiResponse<T> {
         return status;
     }
 
-    public T data() {
+    public T getData() {
         return data;
     }
 }
--- src/main/java/com/example/repetitivecrud/service/AuthService.java
+++ src/main/java/com/example/repetitivecrud/service/AuthService.java
@@ -26,7 +26,7 @@ public class AuthService {
     }
 
 
-    public MemberLoginResponse login(MemberLoginRequest memberLoginRequest){
+    public MemberLoginResponse login(MemberLoginRequest memberLoginRequest) {
         log.info("AuthService.login()");
         // 1. 데이터 준비
         String email = memberLoginRequest.getEmail();
@@ -34,29 +34,39 @@ public class AuthService {
 
         // 2. 회원조회 - 받은 email 로 회원을 검색하기
         Member foundMember = memberRepository.findByEmail(email)
-                .orElseThrow(()-> new RuntimeException("member not found"));
+                .orElseThrow(() -> new RuntimeException("member not found"));
 
         // 3. 비밀번호 검증 - 받아온 비밀번호와 기존 회원의 비밀번호가 일치하는 확인
         String encodedPassword = foundMember.getPassword();
         boolean match = passwordEncoder.matches(password, encodedPassword);
 
-        if(!match){
+        if (!match) {
             throw new RuntimeException("invalid credentials");
         }
         // 로그인 다 끝나고 토큰이 필요한 것임! -> 이제 토큰을 만들려면 JWT를 학습해야함.
         /// 숙제
         // 4. 토큰 만들기
-                   //?????? 토큰에 어떤데이터를 담아줘야할까 ?
-                   // password 절대x , - email, name, id(pk: 식별자)
+        //?????? 토큰에 어떤데이터를 담아줘야할까 ?
+        // password 절대x , - email, name, id(pk: 식별자)
 
 
         Long foundMemberId = foundMember.getId();
         String foundMemberEmail = foundMember.getEmail();
         String foundMemberName = foundMember.getName();
 
+        log.info("==== 토큰 생성 시작 ====");
+        log.info("회원 ID: {}", foundMemberId);
+        log.info("회원 이메일: {}", foundMemberEmail);
+        log.info("회원 이름: {}", foundMemberName);
+
         String token = jwtService.createToken(foundMemberId, foundMemberEmail, foundMemberName);
+
+
 //        String token = "ThisIsJwtToken....";
         MemberLoginResponse responseDto = new MemberLoginResponse(token);
+
+
+
         return responseDto;
 
         // 인증인가
